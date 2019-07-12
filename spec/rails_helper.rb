37e812b3d9d0aea87cc12c7397dfcd1a409348fd
ViKriP <viktor_biz@ukr.net>
2019-07-12 17:06:05 +0300

    initial configuration

---
+++ spec/rails_helper.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../../config/environment', __FILE__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+
+require 'dox'
+require 'rspec/rails'
+require 'factory_bot_rails'
+require 'shoulda-matchers'
+require "pundit/rspec"
+require 'faker'
+
+
+Dir[Rails.root.join('spec/docs/**/*.rb')].each { |f| require f }
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include FactoryBot::Syntax::Methods
+  config.include Shoulda::Matchers::ActiveModel, type: :model
+  config.include Shoulda::Matchers::ActiveRecord, type: :model
+  config.include Shoulda::Matchers::ActionController
+  config.include Shoulda::Matchers::ActiveRecord
+
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+
+  config.after(:each, :dox) do |example|
+    example.metadata[:request] = request
+    example.metadata[:response] = response
+  end
+
+  config.filter_rails_from_backtrace!
+end
+
+Dox.configure do |config|
+  config.header_file_path = Rails.root.join('spec/docs/v1/descriptions/header.md')
+  config.desc_folder_path = Rails.root.join('spec/docs/v1/descriptions')
+  config.headers_whitelist = ['Accept', 'X-Auth-Token']
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
