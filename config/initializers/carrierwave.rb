5d351203fc2422cb6f2293b65d7c8f79ebc7b687
ViKriP <viktor_biz@ukr.net>
2020-01-29 14:47:46 +0200

    fixed credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,13 +3,13 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:access_key],
-      region: Rails.application.credentials.aws[:region],
+      aws_access_key_id: Rails.application.credentials.aws_key_id,
+      aws_secret_access_key: Rails.application.credentials.aws_access_key,
+      region: Rails.application.credentials.aws_region,
       path_style: true
     }
     config.storage = :fog
-    config.fog_directory = Rails.application.credentials.aws[:bucket]
+    config.fog_directory = Rails.application.credentials.aws_s3_bucket
   else
     config.storage = :file
   end
